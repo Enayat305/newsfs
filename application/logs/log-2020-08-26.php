438edb4d99a1b685499c92beb8792543d7b6b811
inaat <inayatullahkks@gmail.com>
2020-08-26 09:43:01 +0500

    ddd

---
+++ application/logs/log-2020-08-26.php
@@ -0,0 +1,208 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2020-08-26 02:31:26 --> Config Class Initialized
+INFO - 2020-08-26 02:31:26 --> Hooks Class Initialized
+DEBUG - 2020-08-26 02:31:26 --> UTF-8 Support Enabled
+INFO - 2020-08-26 02:31:26 --> Utf8 Class Initialized
+INFO - 2020-08-26 02:31:26 --> URI Class Initialized
+DEBUG - 2020-08-26 02:31:26 --> No URI present. Default controller set.
+INFO - 2020-08-26 02:31:26 --> Router Class Initialized
+INFO - 2020-08-26 02:31:26 --> Output Class Initialized
+INFO - 2020-08-26 02:31:26 --> Security Class Initialized
+DEBUG - 2020-08-26 02:31:26 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-08-26 02:31:26 --> CSRF cookie sent
+INFO - 2020-08-26 02:31:26 --> Input Class Initialized
+INFO - 2020-08-26 02:31:26 --> Language Class Initialized
+INFO - 2020-08-26 02:31:26 --> Loader Class Initialized
+INFO - 2020-08-26 02:31:26 --> Helper loaded: url_helper
+INFO - 2020-08-26 02:31:26 --> Helper loaded: file_helper
+INFO - 2020-08-26 02:31:26 --> Helper loaded: form_helper
+INFO - 2020-08-26 02:31:26 --> Helper loaded: security_helper
+INFO - 2020-08-26 02:31:26 --> Helper loaded: directory_helper
+INFO - 2020-08-26 02:31:26 --> Helper loaded: general_helper
+INFO - 2020-08-26 02:31:26 --> Database Driver Class Initialized
+INFO - 2020-08-26 02:31:27 --> Session: Class initialized using 'database' driver.
+INFO - 2020-08-26 02:31:27 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2020-08-26 02:31:27 --> Pagination Class Initialized
+INFO - 2020-08-26 02:31:27 --> XML-RPC Class Initialized
+INFO - 2020-08-26 02:31:27 --> Form Validation Class Initialized
+INFO - 2020-08-26 02:31:27 --> Upload Class Initialized
+INFO - 2020-08-26 02:31:27 --> MY_Model class loaded
+INFO - 2020-08-26 02:31:27 --> Model "Application_model" initialized
+INFO - 2020-08-26 02:31:27 --> Controller Class Initialized
+INFO - 2020-08-26 02:31:27 --> Model "Home_model" initialized
+INFO - 2020-08-26 02:31:27 --> Helper loaded: custom_fields_helper
+INFO - 2020-08-26 02:31:27 --> Model "Email_model" initialized
+INFO - 2020-08-26 02:31:27 --> Model "Testimonial_model" initialized
+INFO - 2020-08-26 02:31:27 --> File loaded: C:\xampp\htdocs\newsfs\application\views\home/index.php
+INFO - 2020-08-26 02:31:27 --> File loaded: C:\xampp\htdocs\newsfs\application\views\home/layout/header.php
+INFO - 2020-08-26 02:31:27 --> File loaded: C:\xampp\htdocs\newsfs\application\views\home/layout/footer.php
+INFO - 2020-08-26 02:31:27 --> File loaded: C:\xampp\htdocs\newsfs\application\views\home/layout/index.php
+INFO - 2020-08-26 02:31:27 --> Final output sent to browser
+DEBUG - 2020-08-26 02:31:27 --> Total execution time: 1.6199
+INFO - 2020-08-26 02:49:58 --> Config Class Initialized
+INFO - 2020-08-26 02:49:58 --> Hooks Class Initialized
+DEBUG - 2020-08-26 02:49:58 --> UTF-8 Support Enabled
+INFO - 2020-08-26 02:49:58 --> Utf8 Class Initialized
+INFO - 2020-08-26 02:49:58 --> URI Class Initialized
+INFO - 2020-08-26 02:49:58 --> Router Class Initialized
+INFO - 2020-08-26 02:49:58 --> Output Class Initialized
+INFO - 2020-08-26 02:49:58 --> Security Class Initialized
+DEBUG - 2020-08-26 02:49:58 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-08-26 02:49:58 --> CSRF cookie sent
+INFO - 2020-08-26 02:49:58 --> Input Class Initialized
+INFO - 2020-08-26 02:49:58 --> Language Class Initialized
+INFO - 2020-08-26 02:49:58 --> Loader Class Initialized
+INFO - 2020-08-26 02:49:58 --> Helper loaded: url_helper
+INFO - 2020-08-26 02:49:58 --> Helper loaded: file_helper
+INFO - 2020-08-26 02:49:58 --> Helper loaded: form_helper
+INFO - 2020-08-26 02:49:58 --> Helper loaded: security_helper
+INFO - 2020-08-26 02:49:58 --> Helper loaded: directory_helper
+INFO - 2020-08-26 02:49:58 --> Helper loaded: general_helper
+INFO - 2020-08-26 02:49:58 --> Database Driver Class Initialized
+INFO - 2020-08-26 02:49:58 --> Session: Class initialized using 'database' driver.
+INFO - 2020-08-26 02:49:58 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2020-08-26 02:49:58 --> Pagination Class Initialized
+INFO - 2020-08-26 02:49:58 --> XML-RPC Class Initialized
+INFO - 2020-08-26 02:49:58 --> Form Validation Class Initialized
+INFO - 2020-08-26 02:49:58 --> Upload Class Initialized
+INFO - 2020-08-26 02:49:58 --> MY_Model class loaded
+INFO - 2020-08-26 02:49:58 --> Model "Application_model" initialized
+INFO - 2020-08-26 02:49:58 --> Controller Class Initialized
+INFO - 2020-08-26 02:49:58 --> Model "Authentication_model" initialized
+INFO - 2020-08-26 02:49:58 --> File loaded: C:\xampp\htdocs\newsfs\application\views\authentication/login.php
+INFO - 2020-08-26 02:49:58 --> Final output sent to browser
+DEBUG - 2020-08-26 02:49:58 --> Total execution time: 0.5669
+INFO - 2020-08-26 02:50:10 --> Config Class Initialized
+INFO - 2020-08-26 02:50:10 --> Hooks Class Initialized
+DEBUG - 2020-08-26 02:50:10 --> UTF-8 Support Enabled
+INFO - 2020-08-26 02:50:10 --> Utf8 Class Initialized
+INFO - 2020-08-26 02:50:10 --> URI Class Initialized
+INFO - 2020-08-26 02:50:10 --> Router Class Initialized
+INFO - 2020-08-26 02:50:10 --> Output Class Initialized
+INFO - 2020-08-26 02:50:10 --> Security Class Initialized
+DEBUG - 2020-08-26 02:50:10 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-08-26 02:50:10 --> CSRF cookie sent
+INFO - 2020-08-26 02:50:10 --> CSRF token verified
+INFO - 2020-08-26 02:50:10 --> Input Class Initialized
+INFO - 2020-08-26 02:50:10 --> Language Class Initialized
+INFO - 2020-08-26 02:50:10 --> Loader Class Initialized
+INFO - 2020-08-26 02:50:10 --> Helper loaded: url_helper
+INFO - 2020-08-26 02:50:10 --> Helper loaded: file_helper
+INFO - 2020-08-26 02:50:10 --> Helper loaded: form_helper
+INFO - 2020-08-26 02:50:10 --> Helper loaded: security_helper
+INFO - 2020-08-26 02:50:10 --> Helper loaded: directory_helper
+INFO - 2020-08-26 02:50:10 --> Helper loaded: general_helper
+INFO - 2020-08-26 02:50:10 --> Database Driver Class Initialized
+INFO - 2020-08-26 02:50:11 --> Session: Class initialized using 'database' driver.
+INFO - 2020-08-26 02:50:11 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2020-08-26 02:50:11 --> Pagination Class Initialized
+INFO - 2020-08-26 02:50:11 --> XML-RPC Class Initialized
+INFO - 2020-08-26 02:50:11 --> Form Validation Class Initialized
+INFO - 2020-08-26 02:50:11 --> Upload Class Initialized
+INFO - 2020-08-26 02:50:11 --> MY_Model class loaded
+INFO - 2020-08-26 02:50:11 --> Model "Application_model" initialized
+INFO - 2020-08-26 02:50:11 --> Controller Class Initialized
+INFO - 2020-08-26 02:50:11 --> Model "Authentication_model" initialized
+INFO - 2020-08-26 02:50:11 --> Language file loaded: language/english/form_validation_lang.php
+INFO - 2020-08-26 02:50:11 --> Config Class Initialized
+INFO - 2020-08-26 02:50:11 --> Hooks Class Initialized
+DEBUG - 2020-08-26 02:50:11 --> UTF-8 Support Enabled
+INFO - 2020-08-26 02:50:11 --> Utf8 Class Initialized
+INFO - 2020-08-26 02:50:11 --> URI Class Initialized
+INFO - 2020-08-26 02:50:11 --> Router Class Initialized
+INFO - 2020-08-26 02:50:11 --> Output Class Initialized
+INFO - 2020-08-26 02:50:11 --> Security Class Initialized
+DEBUG - 2020-08-26 02:50:11 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-08-26 02:50:11 --> CSRF cookie sent
+INFO - 2020-08-26 02:50:11 --> Input Class Initialized
+INFO - 2020-08-26 02:50:11 --> Language Class Initialized
+INFO - 2020-08-26 02:50:11 --> Loader Class Initialized
+INFO - 2020-08-26 02:50:11 --> Helper loaded: url_helper
+INFO - 2020-08-26 02:50:11 --> Helper loaded: file_helper
+INFO - 2020-08-26 02:50:11 --> Helper loaded: form_helper
+INFO - 2020-08-26 02:50:11 --> Helper loaded: security_helper
+INFO - 2020-08-26 02:50:11 --> Helper loaded: directory_helper
+INFO - 2020-08-26 02:50:11 --> Helper loaded: general_helper
+INFO - 2020-08-26 02:50:11 --> Database Driver Class Initialized
+INFO - 2020-08-26 02:50:11 --> Session: Class initialized using 'database' driver.
+INFO - 2020-08-26 02:50:11 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2020-08-26 02:50:11 --> Pagination Class Initialized
+INFO - 2020-08-26 02:50:11 --> XML-RPC Class Initialized
+INFO - 2020-08-26 02:50:11 --> Form Validation Class Initialized
+INFO - 2020-08-26 02:50:11 --> Upload Class Initialized
+INFO - 2020-08-26 02:50:11 --> MY_Model class loaded
+INFO - 2020-08-26 02:50:11 --> Model "Application_model" initialized
+INFO - 2020-08-26 02:50:11 --> Controller Class Initialized
+INFO - 2020-08-26 02:50:11 --> Model "Dashboard_model" initialized
+INFO - 2020-08-26 02:50:11 --> File loaded: C:\xampp\htdocs\newsfs\application\views\layout/header.php
+INFO - 2020-08-26 02:50:12 --> File loaded: C:\xampp\htdocs\newsfs\application\views\layout/topbar.php
+INFO - 2020-08-26 02:50:13 --> File loaded: C:\xampp\htdocs\newsfs\application\views\layout/sidebar.php
+INFO - 2020-08-26 02:50:13 --> File loaded: C:\xampp\htdocs\newsfs\application\views\dashboard/index.php
+INFO - 2020-08-26 02:50:13 --> File loaded: C:\xampp\htdocs\newsfs\application\views\layout/script.php
+INFO - 2020-08-26 02:50:13 --> File loaded: C:\xampp\htdocs\newsfs\application\views\layout/index.php
+INFO - 2020-08-26 02:50:13 --> Final output sent to browser
+DEBUG - 2020-08-26 02:50:13 --> Total execution time: 2.6733
+INFO - 2020-08-26 02:50:14 --> Config Class Initialized
+INFO - 2020-08-26 02:50:14 --> Hooks Class Initialized
+DEBUG - 2020-08-26 02:50:14 --> UTF-8 Support Enabled
+INFO - 2020-08-26 02:50:14 --> Utf8 Class Initialized
+INFO - 2020-08-26 02:50:14 --> URI Class Initialized
+INFO - 2020-08-26 02:50:14 --> Router Class Initialized
+INFO - 2020-08-26 02:50:14 --> Output Class Initialized
+INFO - 2020-08-26 02:50:14 --> Security Class Initialized
+DEBUG - 2020-08-26 02:50:14 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-08-26 02:50:14 --> CSRF cookie sent
+INFO - 2020-08-26 02:50:14 --> Input Class Initialized
+INFO - 2020-08-26 02:50:14 --> Language Class Initialized
+INFO - 2020-08-26 02:50:14 --> Loader Class Initialized
+INFO - 2020-08-26 02:50:14 --> Helper loaded: url_helper
+INFO - 2020-08-26 02:50:14 --> Helper loaded: file_helper
+INFO - 2020-08-26 02:50:14 --> Helper loaded: form_helper
+INFO - 2020-08-26 02:50:14 --> Helper loaded: security_helper
+INFO - 2020-08-26 02:50:14 --> Helper loaded: directory_helper
+INFO - 2020-08-26 02:50:14 --> Helper loaded: general_helper
+INFO - 2020-08-26 02:50:14 --> Database Driver Class Initialized
+INFO - 2020-08-26 02:50:14 --> Session: Class initialized using 'database' driver.
+INFO - 2020-08-26 02:50:14 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2020-08-26 02:50:14 --> Pagination Class Initialized
+INFO - 2020-08-26 02:50:14 --> XML-RPC Class Initialized
+INFO - 2020-08-26 02:50:14 --> Form Validation Class Initialized
+INFO - 2020-08-26 02:50:14 --> Upload Class Initialized
+INFO - 2020-08-26 02:50:14 --> MY_Model class loaded
+INFO - 2020-08-26 02:50:14 --> Model "Application_model" initialized
+INFO - 2020-08-26 02:50:14 --> Controller Class Initialized
+INFO - 2020-08-26 02:50:14 --> File loaded: C:\xampp\htdocs\newsfs\application\views\errors/error_404_message.php
+INFO - 2020-08-26 02:50:14 --> Final output sent to browser
+DEBUG - 2020-08-26 02:50:14 --> Total execution time: 0.9000
+INFO - 2020-08-26 02:50:15 --> Config Class Initialized
+INFO - 2020-08-26 02:50:15 --> Hooks Class Initialized
+DEBUG - 2020-08-26 02:50:15 --> UTF-8 Support Enabled
+INFO - 2020-08-26 02:50:15 --> Utf8 Class Initialized
+INFO - 2020-08-26 02:50:15 --> URI Class Initialized
+INFO - 2020-08-26 02:50:15 --> Router Class Initialized
+INFO - 2020-08-26 02:50:15 --> Output Class Initialized
+INFO - 2020-08-26 02:50:15 --> Security Class Initialized
+DEBUG - 2020-08-26 02:50:15 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-08-26 02:50:15 --> CSRF cookie sent
+INFO - 2020-08-26 02:50:15 --> Input Class Initialized
+INFO - 2020-08-26 02:50:15 --> Language Class Initialized
+INFO - 2020-08-26 02:50:15 --> Loader Class Initialized
+INFO - 2020-08-26 02:50:15 --> Helper loaded: url_helper
+INFO - 2020-08-26 02:50:15 --> Helper loaded: file_helper
+INFO - 2020-08-26 02:50:16 --> Helper loaded: form_helper
+INFO - 2020-08-26 02:50:16 --> Helper loaded: security_helper
+INFO - 2020-08-26 02:50:16 --> Helper loaded: directory_helper
+INFO - 2020-08-26 02:50:16 --> Helper loaded: general_helper
+INFO - 2020-08-26 02:50:16 --> Database Driver Class Initialized
+INFO - 2020-08-26 02:50:16 --> Session: Class initialized using 'database' driver.
+INFO - 2020-08-26 02:50:16 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2020-08-26 02:50:16 --> Pagination Class Initialized
+INFO - 2020-08-26 02:50:16 --> XML-RPC Class Initialized
+INFO - 2020-08-26 02:50:16 --> Form Validation Class Initialized
+INFO - 2020-08-26 02:50:16 --> Upload Class Initialized
+INFO - 2020-08-26 02:50:16 --> MY_Model class loaded
+INFO - 2020-08-26 02:50:16 --> Model "Application_model" initialized
+INFO - 2020-08-26 02:50:16 --> Controller Class Initialized
+INFO - 2020-08-26 02:50:16 --> Final output sent to browser
+DEBUG - 2020-08-26 02:50:16 --> Total execution time: 0.6080
